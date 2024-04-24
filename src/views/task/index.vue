2a47821252894acb3620586ccdb545b1d617ba18
小剑 <i@xiangjianfeng.com>
2024-04-24 11:04:32 +0800

    v1

--- src/views/task/index.vue
+++ src/views/task/index.vue
@@ -6,11 +6,9 @@ const form = reactive({
 </script>
 
 <template>
-  <div>
-    <div class="flex items-center justify-between px-4">
-      <div class="flex items-center h-11 text-xl shrink-0">
-        任务进度
-      </div>
+  <div class="flex flex-col h-full">
+    <div class="flex items-center justify-between h-12 shrink-0 px-5">
+      <div class="flex items-center h-11 text-xl shrink-0" />
       <TaskFilter :form="form" />
     </div>
     <task-table :form="form" />
